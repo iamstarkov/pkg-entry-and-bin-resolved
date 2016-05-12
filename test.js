d070b5b9c419694436ef54eb457bd006fc5bad44
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-12 15:55:40 +0200

    reorganaize fixtures

--- test.js
+++ test.js
@@ -9,8 +9,8 @@ const preCwd = i => path.join(process.cwd(), i);
 const mapPreCwd = R.map(preCwd);
 
 test('entry', async t => {
-  const _ = await pkgEntryAndBinResolved('./fixtures/entry/package.json');
-  t.deepEqual(_, mapPreCwd(['./fixtures/entry/yo.js']));
+  const _ = await pkgEntryAndBinResolved('./fixtures/entry.json');
+  t.deepEqual(_, mapPreCwd(['./fixtures/entry-main.js']));
 });
 
 test('empty input', t => t.throws(pkgEntryAndBinResolved(), TypeError));
